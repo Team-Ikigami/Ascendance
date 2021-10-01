7ee18b3b72afd1e4d314ce3ee52b61d020cb6952
DuckEater54 <DuckEater54@SHITTY-PC>
2021-10-01 11:50:21 +0800

    working on another project

--- data/src/user-interfaces/src/game/openingf.rs
+++ data/src/user-interfaces/src/game/openingf.rs
@@ -1,9 +1,9 @@
 pub mod openingc;
 use rg3d_ui::*
+use rg3d::core::ResourceManager
 
-pub mod Frontpage {
-    pub fn 1() {
+impl Frontpage {
+    async fn one() -> Self {
         // TODO find out hwat enums are, use them to make al th ebttons that will be on the first page of opening the game for the second time and create the pages that open from that
-        impl<M: MessageData, C: Control<M, C>> Button<M, C>
     }
 }
