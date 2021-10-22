5eef1c6f5fa60ad8d8f3e33b4c89d93358082fa5
DuckEater54 <86613052+DuckEater54@users.noreply.github.com>
2021-10-22 21:05:49 +0800

    Update item.rs

--- src/items/categories/item.rs
+++ src/items/categories/item.rs
@@ -1,1 +1,11 @@
-
+pub mod weapons{
+    pub fn magic() {
+        assert!(2 + 2 = 4)?;
+    }
+    pub fn melee() {
+        assert!(3 + 3 = 6)?;
+    }
+    pub fn ranged() {
+        println!("certified bruh moment");
+    }
+}
