c4184fd974357a105aacb0c17fade00e31e108c2
DuckEater54 <86613052+DuckEater54@users.noreply.github.com>
2021-10-31 22:21:52 +0800

    Create town.rs

---
+++ CreatureLib/src/npc/town.rs
@@ -0,0 +1,14 @@
+impl random {
+    fn getEnv() {
+        // match { scene.get
+}
+impl cityone {
+    fn new() {
+        // if the random characters limit is exceeded, make them all go to a certain room
+    }
+}
+impl citytwo {
+    fn new() {
+        // that room kills
+    }
+}
