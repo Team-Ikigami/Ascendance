ea83ce316225c4e223e16c044072525424282278
DuckEater54 <86613052+DuckEater54@users.noreply.github.com>
2021-11-04 23:32:50 +0800

    Update main.rs
    not finished yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::{level::Level, player::Player};
 use rg3d::{
     core::{color::Color, futures::executor::block_on, pool::Handle},
     engine::framework::{Framework, GameEngine, GameState},
@@ -5,24 +6,57 @@ use rg3d::{
     scene::Scene,
 };
 
+mod level;
+mod player;
 struct Game {
-    // Empty for now.
+    scene: Handle<Scene>,
+    level: Level,
+    player: Player,
 }
-
+#[derive(Default)]
+struct InputController {
+    walk_forward: bool,
+    walk_backward: bool,
+    walk_left: bool,
+    walk_right: bool,
+}
 impl GameState for Game {
     fn init(engine: &mut GameEngine) -> Self
     where
         Self: Sized,
     {
-        Self {}
+        let mut scene = Scene::new();
+        scene.ambient_lighting_color = Color::opaque(150, 150, 150);
+        let player = block_on(Player::new(engine.resource_manager.clone(), &mut scene));
+        Self {
+            player,
+            level: block_on(Level::new(engine.resource_manager.clone(), &mut scene)),
+            scene: engine.scenes.add(scene),
+        }
+    }
+    fn on_tick(&mut self, engine: &mut GameEngine, dt: f32) {
+        let scene = &mut engine.scenes[self.scene];
+
+        self.player.update(scene);
     }
 
-    fn on_tick(&mut self, engine: &mut GameEngine, dt: f32) {
-        // This method is called at fixed rate of 60 FPS.
-        // It will contain all the logic of the game.
+    fn on_device_event(
+        &mut self,
+        _engine: &mut GameEngine,
+        _device_id: DeviceId,
+        event: DeviceEvent,
+    ) {
+        self.player.handle_device_event(&event);
+    }
+    fn on_window_event(&mut self, _engine: &mut GameEngine, event: WindowEvent) {
+        match event {
+            WindowEvent::KeyboardInput { input, .. } => {
+                self.player.handle_key_event(&input);
+            }
+            _ => (),
+        }
     }
 }
-
 fn main() {
     Framework::<Game>::new().unwrap().title("RPG").run()
 }
