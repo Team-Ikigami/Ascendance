33d66db5ea28366d90ec95fd030d94f9cf0a453f
DuckEater54 <ikigami.business@gmail.com>
2021-12-29 20:40:40 +0800

    Going to start copying veloren code for 'learning purposes' :)

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,3 +1,24 @@
+#![deny(unsafe_code)]
+
+pub mod addr;
+pub mod cmd;
+pub mod error;
+
+pub use crate::error::Error;
+pub use authc::AuthClientError;
+pub use common_net::msg::ServerInfo;
+pub use specs::{
+    join::Join,
+    saveload::{Marker, MarkerAllocator},
+    Builder,
+    DispatcherBuilder,
+    Entity as EcsEntity,
+    ReadStorage,
+    World,
+    WorldExt,
+};
+use crate::addr::ConnectionArgs;
+
 #[cfg(test)]
 mod tests {
     #[test]
