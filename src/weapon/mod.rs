7468028b1160f9669eac917f34e3a2102eafd20a
DuckEater54 <ikigami.business@gmail.com>
2022-02-12 19:09:24 +0800

    Minor Change

---
+++ src/weapon/mod.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, Copy, Clone)]
+pub struct AttackTypes {
+	pub fire: bool,
+	pub water: bool,
+	pub cold: bool,
+	pub lightning: bool,
+	pub poison: bool,
+	pub psychic: bool,
+	pub acid: bool,
+	pub necrotic: bool,
+	pub bash: bool,
+	pub piercing: bool,
+	pub slashing: bool,
+}
