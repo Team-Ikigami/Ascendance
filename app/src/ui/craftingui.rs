6b66bbd30e29fb8568d13e09b9183e0ea25f3f36
DuckEater54 <ikigami.business@gmail.com>
2022-03-06 21:49:06 +0800

    Add scrollbar builder

--- app/src/ui/craftingui.rs
+++ app/src/ui/craftingui.rs
@@ -7,6 +7,7 @@ use rg3d::gui::{
         GridBuilder,
         Row,
         Column,
+		GridDimension,
     },
     image::ImageBuilder,
     menu::{MenuItemBuilder, MenuBuilder},
@@ -14,14 +15,15 @@ use rg3d::gui::{
     VerticalAlignment,
     HorizontalAlignment,
     UserInterface,
+	scrollbar::{ScrollBarBuilder},
+	Thickness,
+	VerticalAlignment
 };
 struct ThreeCoreBasicBrewingTable {
-	icon: Handle<UiNode>,
-	slot_1: Handle<UiNode>,
-	slot_2: Handle<UiNode>,
-	slot_3: Handle<UiNode>,
+	icons: Vec<Handle<UiNode>>,
 	fuel_slot: Handle<UiNode>,
 	base: Handle<UiNode>,
+	scrolling: Handle<UiNode>,
 }
 struct ThreeCoreMediumBrewingTable;
 struct ThreeCoreAdvancedBrewingTable;
@@ -50,18 +52,134 @@ struct RefinedGodlyAnvil;
 struct NobleBasicAnvil;
 struct NobleMediumAnvil;
 struct NobleAdvancedAnvil;
-struct NobleGodlyAnvil
+struct NobleGodlyAnvil;
+
+pub struct ScrollBarData {
+	pub min: f32,
+	pub max: f32,
+	pub value: f32,
+	pub step: f32,
+	pub row: usize,
+	pub column: usize,
+	pub margin: Thickness,
+	pub show_value: bool,
+	pub orientation: Orientation,
+}
+
+fn IconScrollbar(ctx: &mut BuildContext, data: ScrollBarData) -> Handle<UiNode> {
+	let mut wb = WidgetBuilder::new();
+	match data.orientation {
+		Orientation::Vertical => wb = wb.with_width(30.0),
+		Orientation::Horizontal => wb = wb.with_height(30.0),
+	}
+	ScrollBarBuilder::new(
+		wb.on_row(data.row)
+			.on_column(data.column)
+			.with_margin(data.margin),
+	)
+	.with_min(data.min)
+	.with_max(data.max)
+	.with_value(data.value)
+	.with_step(data.step)
+	.show_value(data.show_value)
+	.with_value_precision(1)
+	.build(ctx);
+
+}
 
 impl ThreeCoreBasicBrewingTable {
     fn new(ui: &mut UserInterface) -> Handle<UiNode> {
         let ctx = &mut ui.build_ctx();
 
-		let icon;
-		let slot_1;
-		let slot_2;
-		let slot_3;
+		let icons;
 		let fuel_slot;
-		let base = GridBuilder::new(WidgetBuilder::new());
+		let scrolling;
+		
+		GridBuilder::new(
+			WidgetBuilder::new()
+			.with_width(1200)
+			.with_height(900)
+			.with_children(
+				GridBuilder::new(
+					WidgetBuilder::new()
+					.on_column(1)
+					.on_height(1)
+					.with_width(840.0)
+					.with_height(1140.0)
+					.with_children(
+						GridBuilder:new(
+							WidgetBuilder::new()
+							.on_column(0)
+							.on_row(0)
+							.with_children(
+								GridBuilder::new(
+									WidgetBuilder::new()
+									.on_row(0)
+								)
+								.add_columns(vec![
+									GridDimension::strict(50.0),
+									GridDimension::strict(150.0),
+									GridDimension::strict(50.0)
+								])
+								.build(ctx),
+								/// item scrolling.
+								GridBuilder::new(
+									WidgetBuilder::new()
+									.on_row(2)
+									.with_child({
+										// rusty-editor scrolling we use it
+										let scrolling = IconScrollbar();
+									})
+									.with_child(
+										Handle::NONE
+									)
+								)
+								.add_rows(vec![
+									GridDimension::strict(50.0),
+									GridDimension::strict(300.0),
+									GridDimension::strict(50.0)
+								])
+								.add_columns(vec![
+									GridDimension::stretch(),
+									GridDimension::strict(30.0)
+								])
+								.build(ctx),
+							)
+						)
+						.add_row(GridDimension::strict(250))
+						.add_row(GridDimension::strict(90))
+						.add_row(GridDimension::strict(800))
+						.build(ctx),
+						GridBuilder::new(
+							WidgetBuilder::new()
+							.on_column(1)
+							.on_row(1)
+							.with_children(
+								Handle::NONE
+							)
+						)
+						.build(ctx),
+					)
+					.build(ctx),
+			)
+			.add_column(GridDimension::strict(570))
+			.add_column(GridDimension::strict(570))
+			.with_opacity(25)
+			.build(ctx)
+		)
+		)
+		.add_columns(vec![
+			GridDimension::strict(30),
+			GridDimension::strict(1140),
+			GridDimension::strict(30)
+		])
+		.add_rows(vec![
+			GridDimension::strict(30),
+			GridDimension::strict(840),
+			GridDimension::strict(30)
+		])
+		.with_opacity(100)
+		.build(ctx);
 
 		Self {
 			icon
